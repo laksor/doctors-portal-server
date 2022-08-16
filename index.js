dcff2f3049ee28952a20c095164b0afc9a2c6c5f
laskor <al.moksud@gmail.com>
2022-08-16 00:24:54 -0400

    server added

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion} = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+const serverName = "Doctor's Portal";
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// connecting mongodb and crud
+
+const uri = 
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pegkils.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+      await client.connect();
+      const serviceCollection = client.db('doctors_portal').collection('services');
+
+        app.get('/service', async(req,res) =>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+
+    } 
+    finally {
+
+    }
+  }
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("Doctor' Portal server");
+});
+  
+app.listen(port, () => {
+    console.log(`${serverName} server is running on port ${port}`);
+});
