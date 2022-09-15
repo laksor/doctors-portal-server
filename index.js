bb09a4d7d3da967cbb642415bf13babc61b657cf
laskor <al.moksud@gmail.com>
2022-09-15 18:31:34 -0400

    user put api added

--- index.js
+++ index.js
@@ -27,12 +27,25 @@ async function run() {
       await client.connect();
       const serviceCollection = client.db('doctors_portal').collection('services');
       const bookingCollection = client.db('doctors_portal').collection('booking');
+      const userCollection = client.db('doctors_portal').collection('user');
 
         app.get('/service', async(req,res) =>{
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        app.put('/user/:email', async(req,res) =>{
+          const email = req.params.email;
+          const user = req.body;
+          const filter = {email: email};
+          const options = { upsert: true };
+          const updateDoc = {
+            $set: user,
+          };
+          const result = await userCollection.updateOne(filter, updateDoc, options);
+          res.send(result);
         })
 
         app.get('/available', async(req,res) =>{
