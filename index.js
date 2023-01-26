cae2614e6dda116affd2c3f2690b6976da201b86
laskor <al.moksud@gmail.com>
2023-01-26 10:52:41 -0500

    payment database added

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ async function run() {
       const bookingCollection = client.db('doctors_portal').collection('booking');
       const userCollection = client.db('doctors_portal').collection('users');
       const doctorCollection = client.db('doctors_portal').collection('doctors');
+      const paymentCollection = client.db('doctors_portal').collection('payments');
 
 
       const verifyAdmin = async (req, res, next) => {
@@ -211,6 +212,22 @@ async function run() {
           console.log('sending email');
           sendBookingEmail(booking);
           return res.send({success: true, result});
+        })
+
+        app.patch('/booking/:id', verifyJWT, async(req, res) =>{
+          const id  = req.params.id;
+          const payment = req.body;
+          const filter = {_id: ObjectId(id)};
+          const updatedDoc = {
+            $set: {
+              paid: true,
+              transactionId: payment.transactionId
+            }
+          }
+    
+          const result = await paymentCollection.insertOne(payment);
+          const updatedBooking = await bookingCollection.updateOne(filter, updatedDoc);
+          res.send(updatedBooking);
         })
 
         app.get('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
